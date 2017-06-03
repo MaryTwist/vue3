a58937694437a9e3dc410af4b3d0f2a1dc2067b8
MaryTwist <isterika-1989@bk.ru>
2017-06-04 00:52:33 +0300

    Widget object. Widget moving. Toolbar template.

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+function Widget(_title, _x, _y, _w, _h) {
+    var self = this;
+
+    this.title = _title;
+    this.x = _x;
+    this.y = _y;
+    this.w = _w;
+    this.h = _h;
+    this.hold = false;
+    this.getLeft = function() { return self.x + "px" };
+    this.getTop = function() { return self.y + "px" };
+    this.getW = function() { return self.w + "px" };
+    this.getH = function() { return self.h + "px" };
+}
+
 var v = new Vue({
     el: "#app1",
     data: {
@@ -5,10 +20,11 @@ var v = new Vue({
         y: 0,
         currentWidget: null,
         widgets: [
-            { hold: false, title: "Loader", x: "250px", y: "100px", w: "120px", h: "75px" },
-            { hold: false, title: "Extractor", x: "300px", y: "130px", w: "120px", h: "75px" },
-            { hold: false, title: "Import to DB", x: "150px", y: "500px", w: "120px", h: "75px" },
-            { hold: false, title: "Rename file", x: "700px", y: "470px", w: "120px", h: "75px" }
+            new Widget("Loader", 200, 120, 125, 120),
+            new Widget("Extractor", 255, 255, 125, 120),
+            new Widget("Import", 255, 385, 125, 120),
+            new Widget("Success", 105, 520, 125, 120),
+            new Widget("Fail", 405, 520, 125, 120)
         ]
     },
 
@@ -26,8 +42,8 @@ var v = new Vue({
             this.y = e.pageY;
 
             if(this.currentWidget != null) {
-                this.currentWidget.x = (e.pageX - this.currentWidget.w >> 1) + "px";
-                this.currentWidget.y = (e.pageY - Math.round(this.currentWidget.h >> 1)) + "px";
+                this.currentWidget.x = (e.pageX - (this.currentWidget.w >> 1));
+                this.currentWidget.y = (e.pageY - (this.currentWidget.h >> 1));
 
                 this.x = this.currentWidget.x;
                 this.y = this.currentWidget.y;
